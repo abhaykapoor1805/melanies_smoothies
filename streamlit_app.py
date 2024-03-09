9b17dae77712a10bf612bfcc41ce55a78ace7713
abhaykapoor1805 <47417139+abhaykapoor1805@users.noreply.github.com>
2024-03-09 13:36:17 +0530

    Update streamlit_app.py
    Adding ingredients for selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
+    # fruityvice
+# st.text(fruityvice_response.json())
+
 # Write directly to the app
 st.title(" :cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -30,10 +33,6 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-    # fruityvice
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 if ingredients_list :
     st.write(ingredients_list)
@@ -41,6 +40,8 @@ if ingredients_list :
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
     st.write(ingredients_string)
     my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order) values('""" + ingredients_string + """','"""+name_on_order+"""')"""
